e537f14ee106c80e2e4774978d43a72fe881a691
mr-scrpt <mr.scrpt@gmail.com>
2021-11-30 12:05:53 +0200

    implemented: several sections

--- src/data/index.js
+++ src/data/index.js
@@ -4,4 +4,5 @@ exports.data = {
 	menu: JSON.parse(readFileSync("./src/data/menu.json", "utf8")),
 	phone: JSON.parse(readFileSync("./src/data/phone.json", "utf8")),
 	social: JSON.parse(readFileSync("./src/data/social.json", "utf8")),
+	work: JSON.parse(readFileSync("./src/data/work.json", "utf8")),
 };
