7a8223965478a0637cf14edbf27af2f4af1e8292
mr-scrpt <mr.scrpt@gmail.com>
2021-12-01 10:52:39 +0200

    implemented: benefits section

--- src/data/index.js
+++ src/data/index.js
@@ -5,4 +5,5 @@ exports.data = {
 	phone: JSON.parse(readFileSync("./src/data/phone.json", "utf8")),
 	social: JSON.parse(readFileSync("./src/data/social.json", "utf8")),
 	work: JSON.parse(readFileSync("./src/data/work.json", "utf8")),
+	benefits: JSON.parse(readFileSync("./src/data/benefits.json", "utf8")),
 };
