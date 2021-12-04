80bc5619fea8697da1081d7bc69ddc4891bc7339
mr-scrpt <mr.scrpt@gmail.com>
2021-12-04 13:57:33 +0200

    implemented: teams section

--- src/data/index.js
+++ src/data/index.js
@@ -8,4 +8,5 @@ exports.data = {
 	benefits: JSON.parse(readFileSync("./src/data/benefits.json", "utf8")),
 	reviews: JSON.parse(readFileSync("./src/data/reviews.json", "utf8")),
 	faq: JSON.parse(readFileSync("./src/data/faq.json", "utf8")),
+	team: JSON.parse(readFileSync("./src/data/team.json", "utf8")),
 };
