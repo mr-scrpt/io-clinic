f875dfb9a6934d3cafd23a11c7ae2d79641e19a2
mr-scrpt <mr.scrpt@gmail.com>
2021-12-07 14:36:53 +0200

    implemented: slider to section works

---
+++ src/js/custom/sliders.js
@@ -0,0 +1,64 @@
+/*const swiper = new Swiper(".swiper", {
+	slidesPerView: 1,
+	spaceBetween: 30,
+	pagination: {
+		el: ".swiper-pagination",
+		type: "bullets",
+		clickable: true,
+	},
+	breakpoints: {
+
+		768: {
+			slidesPerView: 3,
+
+		},
+
+		994: {
+			slidesPerView: 3,
+		},
+		1280: {
+			slidesPerView: 3,
+			navigation: {
+				nextEl: ".slider__nav_next",
+				prevEl: ".slider__nav_prev",
+			},
+		},
+	},
+});*/
+
+$(document).ready(() => {
+	$sliders = $(".swiper");
+
+	$sliders.each((_, swiperItem) => {
+		const sliderBox = swiperItem.closest(".slider");
+		const pagination = sliderBox.querySelector(".slider__pagination");
+		const navNext = sliderBox.querySelector(".slider__nav_next");
+		const navPrev = sliderBox.querySelector(".slider__nav_prev");
+
+		const swiper = new Swiper(swiperItem, {
+			slidesPerView: 1,
+			spaceBetween: 30,
+			pagination: {
+				el: pagination,
+				type: "bullets",
+				clickable: true,
+			},
+			breakpoints: {
+				768: {
+					slidesPerView: 3,
+				},
+
+				994: {
+					slidesPerView: 3,
+				},
+				1280: {
+					slidesPerView: 3,
+					navigation: {
+						nextEl: navNext,
+						prevEl: navPrev,
+					},
+				},
+			},
+		});
+	});
+});
