1dc1f27cffb3526f12d0befa57692f348091b99f
mr-scrpt <mr.scrpt@gmail.com>
2021-12-09 13:12:59 +0200

    implemented: video section on desktop

--- src/js/custom/sliders.js
+++ src/js/custom/sliders.js
@@ -30,36 +30,54 @@ $(document).ready(() => {
 	$sliders = $(".swiper");
 
 	$sliders.each((_, swiperItem) => {
+		const optionName = $(swiperItem).attr("data-option");
 		const sliderBox = swiperItem.closest(".slider");
-		const pagination = sliderBox.querySelector(".slider__pagination");
 		const navNext = sliderBox.querySelector(".slider__nav_next");
 		const navPrev = sliderBox.querySelector(".slider__nav_prev");
-
-		const swiper = new Swiper(swiperItem, {
-			slidesPerView: 1,
-			spaceBetween: 30,
-			autoHeight: true,
-			pagination: {
-				el: pagination,
-				type: "bullets",
-				clickable: true,
-			},
-			breakpoints: {
-				768: {
-					slidesPerView: 3,
+		const pagination = sliderBox.querySelector(".slider__pagination");
+		if (optionName === "optionCommon") {
+			new Swiper(swiperItem, getOptions(navNext, navPrev, pagination));
+		}
+		if (optionName === "optionMain") {
+			new Swiper(swiperItem, {
+				slidesPerView: 1,
+				centeredSlides: true,
+				loop: true,
+				_spaceBetween: 30,
+				autoHeight: true,
+				centeredSlides: true,
+				navigation: {
+					nextEl: navNext || null,
+					prevEl: navPrev || null,
 				},
-
-				994: {
-					slidesPerView: 3,
-				},
-				1280: {
-					slidesPerView: 3,
-					navigation: {
-						nextEl: navNext,
-						prevEl: navPrev,
-					},
-				},
-			},
-		});
+			});
+		}
 	});
+});
+
+const getOptions = (navNext, navPrev, pagination) => ({
+	slidesPerView: 1,
+	spaceBetween: 30,
+	autoHeight: true,
+	pagination: {
+		el: pagination || null,
+		type: "bullets",
+		clickable: true,
+	},
+	breakpoints: {
+		768: {
+			slidesPerView: 3,
+		},
+
+		994: {
+			slidesPerView: 3,
+		},
+		1280: {
+			slidesPerView: 3,
+			navigation: {
+				nextEl: navNext || null,
+				prevEl: navPrev || null,
+			},
+		},
+	},
 });
