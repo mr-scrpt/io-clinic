82894235c7733b0073e4063fbbc3b8858d041c6b
mr-scrpt <mr.scrpt@gmail.com>
2021-12-09 16:16:04 +0200

    implemented: video section

--- src/js/custom/sliders.js
+++ src/js/custom/sliders.js
@@ -1,83 +1,83 @@
-/*const swiper = new Swiper(".swiper", {
-	slidesPerView: 1,
-	spaceBetween: 30,
-	pagination: {
-		el: ".swiper-pagination",
-		type: "bullets",
-		clickable: true,
-	},
-	breakpoints: {
-
-		768: {
-			slidesPerView: 3,
-
-		},
-
-		994: {
-			slidesPerView: 3,
-		},
-		1280: {
-			slidesPerView: 3,
-			navigation: {
-				nextEl: ".slider__nav_next",
-				prevEl: ".slider__nav_prev",
-			},
-		},
-	},
-});*/
-
 $(document).ready(() => {
-	$sliders = $(".swiper");
+	/*reviews*/
+	const reviews = document.querySelector(".swiper.reviews__inner");
+	const reviewsBox = reviews.closest(".slider");
+	const reviewsNavNext = reviewsBox.querySelector(".slider__nav_next");
+	const reviewsNavPrev = reviewsBox.querySelector(".slider__nav_prev");
+	const reviewsPagination = reviewsBox.querySelector(".slider__pagination");
 
-	$sliders.each((_, swiperItem) => {
-		const optionName = $(swiperItem).attr("data-option");
-		const sliderBox = swiperItem.closest(".slider");
-		const navNext = sliderBox.querySelector(".slider__nav_next");
-		const navPrev = sliderBox.querySelector(".slider__nav_prev");
-		const pagination = sliderBox.querySelector(".slider__pagination");
-		if (optionName === "optionCommon") {
-			new Swiper(swiperItem, getOptions(navNext, navPrev, pagination));
-		}
-		if (optionName === "optionMain") {
-			new Swiper(swiperItem, {
-				slidesPerView: 1,
-				centeredSlides: true,
-				loop: true,
-				_spaceBetween: 30,
-				autoHeight: true,
-				centeredSlides: true,
-				navigation: {
-					nextEl: navNext || null,
-					prevEl: navPrev || null,
-				},
-			});
-		}
-	});
+	const optionReviews = getOptionReviews(
+		reviewsPagination,
+		reviewsNavNext,
+		reviewsNavPrev
+	);
+	sliderGen(reviews, optionReviews);
+
+	/*worklist*/
+	const worklist = document.querySelector(".swiper.worklist__inner");
+	const workBox = worklist.closest(".slider-vertical");
+	const workNavNext = workBox.querySelector(".slider-vertical__nav_next");
+	const workNavPrev = workBox.querySelector(".slider-vertical__nav_prev");
+
+	const optionWork = getOptionWork(workNavNext, workNavPrev);
+	sliderGen(worklist, optionWork);
 });
 
-const getOptions = (navNext, navPrev, pagination) => ({
-	slidesPerView: 1,
-	spaceBetween: 30,
-	autoHeight: true,
-	pagination: {
-		el: pagination || null,
-		type: "bullets",
-		clickable: true,
-	},
-	breakpoints: {
-		768: {
-			slidesPerView: 3,
-		},
+const sliderGen = (block, option) => {
+	new Swiper(block, option);
+};
 
-		994: {
-			slidesPerView: 3,
+const getOptionReviews = (pagination, navNext, navPrev) => {
+	return {
+		slidesPerView: 1,
+		spaceBetween: 30,
+		autoHeight: true,
+		pagination: {
+			el: pagination,
+			type: "bullets",
+			clickable: true,
 		},
-		1280: {
-			slidesPerView: 3,
-			navigation: {
-				nextEl: navNext || null,
-				prevEl: navPrev || null,
+		breakpoints: {
+			768: {
+				slidesPerView: 3,
 			},
+
+			994: {
+				slidesPerView: 3,
+			},
+			1280: {
+				slidesPerView: 3,
+				navigation: {
+					nextEl: navNext,
+					prevEl: navPrev,
+				},
+			},
+		},
+	};
+};
+
+const getOptionWork = (navNext, navPrev) => ({
+	direction: "vertical",
+	slidesPerView: 2,
+	spaceBetween: 30,
+	grid: {
+		rows: 1,
+		/*column: 2,*/
+		fill: "row",
+	},
+
+	autoHeight: false,
+	navigation: {
+		nextEl: navNext,
+		prevEl: navPrev,
+	},
+
+	994: {
+		slidesPerView: 2,
+		grid: {
+			rows: 2,
+			/*column: 2,*/
+			fill: "row",
 		},
 	},
 });
